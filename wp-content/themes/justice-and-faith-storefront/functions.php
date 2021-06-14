283f95758f9432cf757df04291cac1786b2eac15
Robin Giese <robin@grumpycorp.com>
2021-06-13 17:07:56 -0700

    Simplify header elements, use flexbox for layout

--- wp-content/themes/justice-and-faith-storefront/functions.php
+++ wp-content/themes/justice-and-faith-storefront/functions.php
@@ -13,5 +13,21 @@
 		echo '<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">' . "\n";
 	}
 
-	add_action('wp_head', 'amend_header');	
+	add_action('wp_head', 'amend_header');
+
+	// Reorder Storefront header
+	add_action('init', 'reorder_storefront_primary_navigation', 15 /* magic value from the internet what could go wrong */);
+
+	function reorder_storefront_primary_navigation() {
+		// Remove search box
+		remove_action('storefront_header', 'storefront_product_search', 40);
+
+		// Remove wrapping outer div for site branding
+		remove_action('storefront_header', 'storefront_header_container', 0);
+		remove_action('storefront_header', 'storefront_header_container_close', 41);
+
+		// Remove wrapping outer div for site navigation
+		remove_action('storefront_header', 'storefront_primary_navigation_wrapper', 42);
+		remove_action('storefront_header', 'storefront_primary_navigation_wrapper_close', 68);
+	}
 ?>
